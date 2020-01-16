d8652fa02af986829926cfc9e39c0c76326135d5
Ally Morson <ally.morson@gmail.com>
2020-01-16 19:50:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = [] #show all of the artists of existing songs
-  @@genres = [] #keep track of the number of songs of each genre it creates
+  @@artists = [] #show all of the artists of existing? songs
+  @@genres = [] #keep track of the number of songs of each genre it creates?
   
   def initialize (name , artist , genre)
     @@song_count += 1 
     @artist = artist
     @genre = genre
     @name = name
+    @@artist << 
   end 
   
   def self.count 
