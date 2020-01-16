d38200801cf148272d7c5fa5301b9852a53f48c9
Ally Morson <ally.morson@gmail.com>
2020-01-16 19:46:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,25 @@
 class Song 
+  
   @@song_count = 0
+ 
+  
   def initialize
     @@song_count += 1 
+    @@artist = []
+    
+  end 
+  
+  def self.count 
+      @@song_count 
+    end 
+    
+  def self.artist 
+    @@artist << self
+  end 
+  
+  def self.genres 
+    self.each do |song| 
+      
+    end 
+    
+    def self.genre_count 
