7562e0fde692b5ab353e8e5a73dc45e48f0282bc
Ally Morson <ally.morson@gmail.com>
2020-01-16 22:36:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,28 +2,25 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = [] #show all of the artists of existing? songs
-  @@genres = [] #keep track of the number of songs of each genre it creates? returns an array of all of the genres of existing songs
-  
-  def initialize (name , artist , genre)
-    @@song_count += 1 
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
     @artist = artist
     @genre = genre
-    @name = name
-    @@genres << genre #keep track of the number of songs of each genre it creates
-    @@artists << artist #show all of the artists of existing
-  end 
-  
-#Now we must call on the class methods to perform the above 
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
 
-  def self.count 
-      @@song_count 
-    end 
-    
-  def self.artist 
-    @@artist.uniq #only contains unique artists - no repeats in the array 
-  end 
-  
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
 
   def self.artist_count
     artist_count = {}
@@ -37,7 +34,8 @@ class Song
     artist_count
   end
 
-    artist_count
+  def self.genres
+    @@genres.uniq
   end
   
   def self.genres
