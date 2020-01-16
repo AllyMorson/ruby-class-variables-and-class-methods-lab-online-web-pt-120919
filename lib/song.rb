e03e75ea0200610db4b1e4f08161c6fdff1efff8
Ally Morson <ally.morson@gmail.com>
2020-01-16 22:35:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,13 +24,17 @@ class Song
     @@artist.uniq #only contains unique artists - no repeats in the array 
   end 
   
-  def self.artist_count 
+def self.artist_count
     artist_count = {}
-    @@artist.each do |artist|
-    if artist_count[artist] = 
-      artist_count[artist] += 1 
-    else 
-      artist_count = 1 
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+
+    artist_count
+  end
   
   def self.genres
     @@genres.uniq
