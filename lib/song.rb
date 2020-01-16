396bd21a641d40f75e2f7125085cefad0f6ba969
Ally Morson <ally.morson@gmail.com>
2020-01-16 22:36:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,8 @@ class Song
   def self.genres
     @@genres.uniq
   end
-    
-    def self.genre_count
+   
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
